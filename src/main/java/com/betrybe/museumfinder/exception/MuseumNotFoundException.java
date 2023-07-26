fd6741cef03975ed849e22fd4953e373d84ca46f
Thiago <thiago-470@outlook.com>
2023-07-25 22:54:16 -0300

    req07

--- src/main/java/com/betrybe/museumfinder/exception/MuseumNotFoundException.java
+++ src/main/java/com/betrybe/museumfinder/exception/MuseumNotFoundException.java
@@ -1,8 +1,7 @@
 package com.betrybe.museumfinder.exception;
 
 /**
- * Uma exceção personalizada que indica que uma coordenada inválida foi fornecida.
- * */
+ * Uma exceção personalizada que indica que uma coordenada inválida foi fornecida. */
 
 public class MuseumNotFoundException extends RuntimeException {
 
